c1bccb051b38036ae41ac3a2765bbf952753ff59
Deepak Janardhanan <deepakramoham@gmail.com>
2024-03-24 10:58:58 +0530

    02-tut-rendering-JSXElements

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const name = "Dave";
 
   return (
     <>
@@ -28,8 +30,27 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>html</p>
+      <p>{"Dave"}</p>
+      <p>Dave</p>
+      <p>"dave"</p>
+      <p>{"1"}</p>
+      <p>"1"</p>
+      <p>1</p>
+      <p>{[1, 2, 3]}</p>
+      <p>[1,2,3]</p>
+     { <p>"[1,2,3]"</p>}
+      <p>{name}</p>
+      <p>name</p>
+      {/*<p> {{a:1,b:2}}</p> {Objects are not valid as a React child}*/}
+      {/*<p>{2===4}</p> */}
+      {/*we use curly braces to put javascript expressions in jsx. however we cannot render objects using curly braces. This would give error */}
+      {/* we cannot  use curly braces to boolean also*/}
+      <p>"shift+alt+a -- to comment"</p>
+      {/*shift+alt+a - > to comment*/}
+      <p></p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
