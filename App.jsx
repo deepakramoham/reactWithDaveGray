c8dd20a12820e82d2ecd91e754b5799792b30622
Deepak Janardhanan <deepakramoham@gmail.com>
2024-03-24 19:42:58 +0530

    07-tut-defaultProps

--- App.jsx
+++ App.jsx
@@ -1,21 +1,18 @@
-
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 
-
 function App() {
-
- /*  npm install --save style-components */
+  /*  npm install --save style-components */
 
   return (
-    <div className = "App">
-      
-      <Header title="Grocery List"/>
-      <Content/>
-      <Footer/>
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
 
+Header.default;
 export default App;
--- components/Header.jsx
+++ components/Header.jsx
@@ -7,4 +7,11 @@ const Header = ({title}) => {
   </header>)
 }
 
+/* Default Props:- Default props allow us to set values for the props expected in the component.
+If not provided default values will take over instead of receiving an error.
+This is great thing to do when you are first designing the component where the data is note receiving which is
+ expected from api/localstorage etc  */
+ Header.defaultProps={title:"Default Title"}
+
+
 export default Header;
