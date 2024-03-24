c8ecd9a01af9e56a930f5613aaf81ac68eb3af08
Deepak Janardhanan <deepakramoham@gmail.com>
2024-03-24 19:34:20 +0530

    07-tut-props&propsDrilling

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,9 @@
 
-const Header = () => {
+const Header = (props) => {
   
-
+/* props are used to pass data from parent to child component */
   return(<header>
-      <h1>Groceries List</h1>
+      <h1>{props.title}</h1>
   </header>)
 }
 
